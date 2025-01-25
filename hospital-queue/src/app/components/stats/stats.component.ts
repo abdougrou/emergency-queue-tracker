671bac702332ace5adb6e9338fd22abf4ab7a18b
grouab <abderrahmane.grou@polymtl.ca>
2025-01-25 15:38:17 -0500

    Add Stats Component

---
+++ hospital-queue/src/app/components/stats/stats.component.ts
@@ -0,0 +1,29 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { Stats } from '../../models/patient.model';
+import { HospitalService } from '../../services/hospital.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stats',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stats.component.html',
+  styleUrl: './stats.component.css'
+})
+export class StatsComponent {
+  private hospitalService = inject(HospitalService);
+  stats = signal<Stats | null>(null);
+  categories = [1, 2, 3, 4, 5];
+
+  constructor() {
+    effect(() => {
+      this.loadStats();
+    });
+  }
+
+  loadStats() {
+    this.hospitalService.getStats().subscribe(
+      data => this.stats.set(data)
+    );
+  }
+}
